2a7fc8401f0bac95d069dd230b39f31d7f2c08f1
Brane105 <brane6963@gmail.com>
2023-09-09 20:14:52 +0530

    add new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,47 @@ import { ServiceComponent } from './service/service.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { IonicModule } from '@ionic/angular';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { NotifierModule, NotifierOptions } from 'angular-notifier';
+const customNotifierOptions: NotifierOptions = {
+  position: {
+		horizontal: {
+			position: 'middle',
+			distance: 12
+		},
+		vertical: {
+			position: 'bottom',
+			distance: 12, 
+			gap: 10
+		}
+	},
+  theme: 'material',
+  behaviour: {
+    autoHide: 5000,
+    onClick: 'hide',
+    onMouseover: 'pauseAutoHide',
+    showDismissButton: true,
+    stacking: 4
+  },
+  animations: {
+    enabled: true,
+    show: {
+      preset: 'slide',
+      speed: 300,
+      easing: 'ease'
+    },
+    hide: {
+      preset: 'fade',
+      speed: 300,
+      easing: 'ease',
+      offset: 50
+    },
+    shift: {
+      speed: 300,
+      easing: 'ease'
+    },
+    overlap: 150
+  }
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,6 +67,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     IonicModule.forRoot(),
     ReactiveFormsModule,
+    NotifierModule.withConfig(customNotifierOptions)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import { NotifierService } from 'angular-notifier';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -13,7 +14,26 @@ export class ContactComponent {
   message: any;
   formdata: any;
 
-  constructor() {}
+  private notifier: NotifierService;
+
+	/**
+	 * Constructor
+	 *
+	 * @param {NotifierService} notifier Notifier service
+	 */
+	public constructor( notifier: NotifierService ) {
+		this.notifier = notifier;
+	}
+
+	/**
+	 * Show a notification
+	 *
+	 * @param {string} type    Notification type
+	 * @param {string} message Notification message
+	 */
+	public showNotification( type: string, message: string ): void {
+		this.notifier.notify( type, message );
+	}
   ngOnInit() {
     this.formdata = new FormGroup({
       from_name: new FormControl(""),
@@ -24,13 +44,23 @@ export class ContactComponent {
  }
   sendEmail(data:any) {
     console.log(data)
-    emailjs.send('service_a18ko9m', 'template_jwhigqe', data,'2ithvLZdAFQsBsBrt')
+    if(data.from_name != '' && data.from_email != '' && data.message != '' && data.subject != ''){
+      emailjs.send('service_a18ko9m', 'template_jwhigqe', data,'2ithvLZdAFQsBsBrt')
       .then((response: EmailJSResponseStatus) => {
         console.log('Email sent:', response);
+        this.showNotification( 'success', 'Email successfully sent!' );
+        this.formdata.reset();
         // Handle success, e.g., show a success message to the user
       }, (error) => {
         console.error('Email sending failed:', error);
+        this.formdata.reset();
+        this.showNotification( 'error', 'Whoops, something went wrong. Probably!' );
         // Handle error, e.g., show an error message to the user
       });
+    }
+    else{
+      this.formdata.reset();
+      this.showNotification( 'error', 'Whoops, Please fill all the details!' );
+    }
   }
 }
